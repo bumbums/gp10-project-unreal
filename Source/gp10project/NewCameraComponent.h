7a41b1cf2cf4df753d86cf97764e996be6d86a8b
박범수 <pbs1219@gmail.com>
2022-11-01 15:13:45 +0900

    1101
    top view camera added

---
+++ Source/gp10project/NewCameraComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Camera/CameraComponent.h"
+#include "NewCameraComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GP10PROJECT_API UNewCameraComponent : public UCameraComponent
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/gp10project/gp10project.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "gp10project.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, gp10project, "gp10project" );
